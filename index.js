c9523b3cf642d893f8b7ab5a1508b4e4c6b58727
gauravpdwl <gauravpadwal45@gmail.com>
2024-08-18 07:34:45 +0530

    updated res to contain origin request headers which was causing cors issue

--- index.js
+++ index.js
@@ -6,13 +6,23 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
+// Handle preflight requests
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.sendStatus(204);
+});
+
 // Apply middleware
 app.use(cors({
-    origin:["http://localhost:5173", "http://localhost:3000/"],
+    origin:["http://localhost:5173", "http://localhost:3000"],
     credentials: true
   }));
 app.use(helmet());
 app.use(morgan('dev'));
+
 
 // Define API Gateway configurations (replace with your actual service details)
 const services = [
@@ -35,7 +45,15 @@ const services = [
 
 // Create proxy middleware for each service
 services.forEach((service) => {
-  app.use(service.path, createProxyMiddleware({ target: service.target }));
+  app.use(service.path, createProxyMiddleware({ 
+    target: service.target,
+    changeOrigin: true,
+    onProxyRes: function (proxyRes, req, res) {
+      // Set CORS headers on the response
+      proxyRes.headers['Access-Control-Allow-Origin'] = req.headers.origin;
+      proxyRes.headers['Access-Control-Allow-Credentials'] = 'true';
+    } 
+  }));
 });
 
 // Error handling middleware (optional)
